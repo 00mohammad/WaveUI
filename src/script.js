479dc8978099d36ef70e1c7788a9ad7ded2578fb
Mohammad Honarmande <mohammadizi1401@gmail.com>
2026-07-23 19:47:30 +0330

    Add Swiper slider for destinations section

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+
+  slidesPerView: 1,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
