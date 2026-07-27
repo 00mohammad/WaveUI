6be7e1642bc9de539c91386194a704b6f3e33f59
Mohammad Honarmande <mohammadizi1401@gmail.com>
2026-07-27 22:10:50 +0330

    Implement mobile-first Swiper layout (tablet pending)

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper(".mySwiper", {
+<<<<<<< HEAD
   loop: false,
   centeredSlides: false,
+=======
+  loop: true,
+
+>>>>>>> 4a476ec (Implement mobile-first Swiper layout (tablet pending))
   slidesPerView: 1,
   spaceBetween: 0,
 
@@ -15,11 +20,14 @@ const swiper = new Swiper(".mySwiper", {
   },
 
   breakpoints: {
+<<<<<<< HEAD
     640: {
       slidesPerView: 1,
       spaceBetween: 0,
     },
 
+=======
+>>>>>>> 4a476ec (Implement mobile-first Swiper layout (tablet pending))
     768: {
       slidesPerView: 2,
       spaceBetween: 45,
@@ -27,7 +35,11 @@ const swiper = new Swiper(".mySwiper", {
 
     1024: {
       slidesPerView: 3,
+<<<<<<< HEAD
       spaceBetween: 90,
+=======
+      spaceBetween: 45,
+>>>>>>> 4a476ec (Implement mobile-first Swiper layout (tablet pending))
     },
   },
 });
